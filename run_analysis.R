83eb2b253bf39b6c8b3923565c5bf7f598beab1e
Shiva Conjeevaram <cssrk2000@gmail.com>
2017-05-01 12:32:32 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+library(reshape2)
+
+setwd("/home/cssrk/Documents/Coursera/Course3/Week4")
+
+filename <- "getdata projectfiles UCI HAR Dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+# Load the activity labels and features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract the mean and SD data only
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+
+
+# Load the datasets to merge them
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# merge train and test datasets and add labels
+mergedData <- rbind(train, test)
+colnames(mergedData) <- c("subject", "activity", featuresWanted.names)
+
+# turn activities & subjects into factors
+mergedData$activity <- factor(mergedData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+mergedData$subject <- as.factor(mergedData$subject)
+
+mergedData.melted <- melt(mergedData, id = c("subject", "activity"))
+mergedData.mean <- dcast(mergedData.melted, subject + activity ~ variable, mean)
+
+write.table(mergedData.mean, "tidy_data.txt", row.names = FALSE, quote = FALSE)
